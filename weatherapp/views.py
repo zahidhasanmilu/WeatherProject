e26734e4022bee81d2b00a4262131cba92eb1fb4
Zahid Hasan Milu <minions.milu889@gmail.com>
2024-03-16 06:06:40 +0600

    initial commit

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,3 +1,44 @@
+import datetime
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method == "GET":
+        city = request.GET.get('city')
+        if not city:  # If the city is blank, use default location (Dhaka)
+            city = 'Dhaka'
+
+    appid = "c82f708fd3aa6ea0a49c877fa1cc01bd"
+    url = "https://api.openweathermap.org/data/2.5/weather"
+
+    parameters = {
+        'q': city,
+        'appid': appid,
+        'units': 'metric'  # Corrected typo
+    }
+
+    try:
+        response = requests.get(url=url, params=parameters)
+        response.raise_for_status()  # Raise exception for unsuccessful responses
+        data = response.json()
+
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+    except requests.exceptions.RequestException as e:
+        # Handle API request errors gracefully
+        description = "Error fetching data: " + str("city Not Found")
+        icon = ""
+        temp = ""
+
+    day = datetime.date.today()
+
+    context = {
+        'description': description,
+        'icon': icon,
+        'temp': temp,
+        'day': day,
+        'city': city
+    }
+
+    return render(request, 'weatherapp/index.html', context)
